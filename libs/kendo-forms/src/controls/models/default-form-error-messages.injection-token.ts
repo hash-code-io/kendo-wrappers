168fdce3fa5ec08606adf856abd9c08ce2b12a39
JejuniProgramming <jejuniprogramming@gmail.com>
2022-11-16 11:44:32 +0100

    fix(kendo-forms): fix circular import

--- libs/kendo-forms/src/controls/models/default-form-error-messages.injection-token.ts
+++ libs/kendo-forms/src/controls/models/default-form-error-messages.injection-token.ts
@@ -1,4 +1,4 @@
 import { InjectionToken } from '@angular/core';
-import { ErrorMessages } from '@hash-code/kendo-forms';
+import { ErrorMessages } from './error-messages';
 
 export const DEFAULT_FORM_ERROR_MESSAGES = new InjectionToken<ErrorMessages>('DEFAULT_FORM_ERROR_MESSAGES');
