e63e076ee563d51d6012168d5baef8b375926a5e
JejuniProgramming <jejuniprogramming@gmail.com>
2022-11-16 19:13:12 +0100

    refactor: added project scope rules

---
+++ libs/kendo-popup/src/lib/popup-manager.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class PopupManagerService {
+  // public constructor(private popupService: PopupService) {}
+  // public open(): void {}
+}
