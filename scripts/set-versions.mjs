e393b1fb3625528d64f8cd3131e966670bd9354c
JejuniProgramming <jejuniprogramming@gmail.com>
2022-11-17 13:50:30 +0100

    feat: updated script with log outputs

--- scripts/set-versions.mjs
+++ scripts/set-versions.mjs
@@ -32,24 +32,32 @@ function* getPackageJsons() {
 const tsconfig = getFileContentAsObject(tsconfigPath);
 const potentialPackagesNames = Object.keys(tsconfig.compilerOptions.paths);
 
+console.log(`looking for the following dependencies: [${potentialPackagesNames.join(', ')}]`);
+console.log();
+
 for (const packageFilePath of getPackageJsons()) {
   const packageContent = getFileContentAsObject(packageFilePath);
   let hasChanges = false;
   for(const dependency in packageContent.peerDependencies) {
     if(potentialPackagesNames.includes(dependency)) {
+      console.log(`found package [${dependency}] in peerDependencies in file [${packageFilePath}]`);
       hasChanges = true;
       packageContent.peerDependencies[dependency] = versionToSet;
     }
   }
   for(const dependency in packageContent.dependencies) {
     if(potentialPackagesNames.includes(dependency)) {
+      console.log(`found package [${dependency}] in dependencies in file [${packageFilePath}]`);
       hasChanges = true;
       packageContent.dependencies[dependency] = versionToSet;
     }
   }
 
+  console.log();
+
   if(hasChanges) {
-    const newFileContent = JSON.stringify(packageContent);
+    console.log(`writing out updated package.json to [${packageFilePath}]`)
+    const newFileContent = JSON.stringify(packageContent, null, 2);
     writeFileSync(packageFilePath, newFileContent, {encoding: 'utf8'});
   }
 }
